2ce662d306f8a6b02e075f88d9d51142dc653819
bntxperses <dbikramyt@gmail.com>
2025-11-22 11:29:30 +0545

    alioth: Clover Bringup

--- clover_alioth.mk
+++ clover_alioth.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Clover stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Inherit Camera-related flags
 TARGET_USES_MIUI_CAMERA := true
@@ -18,7 +18,12 @@ TARGET_INCLUDES_MIUI_CAMERA := true
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+# Clover
+CLOVER_BUILDTYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+CLOVER_MAINTAINER := Perses
+
+PRODUCT_NAME := clover_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
+
+git clone https://github.com/PocoF3Releases/vendor_qcom_wfd.git vendor/qcom/wfd
+git clone https://github.com/PocoF3Releases/device_qcom_wfd.git device/qcom/wfd
+git clone https://github.com/Project-Persesx/device_xiaomi_sm8250-common.git  -b halcyon device/xiaomi/sm8250-common
+git clone  --depth=1 https://github.com/Project-Persesx/vendor_xiaomi_sm8250-common.git -b halcyon vendor/xiaomi/sm8250-common
+git clone https://github.com/Project-Persesx/vendor_xiaomi_alioth.git vendor/xiaomi/alioth
+git clone https://github.com/Project-Persesx/hardware_xiaomi.git hardware/xiaomi
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings 
+git clone https://github.com/PocoF3Releases/device_xiaomi_camera.git device/xiaomi/camera
+git clone https://github.com/Project-Persesx/hardware_dolby.git hardware/dolby
+git clone --depth=1 https://gitlab.com/johnmart19/vendor_xiaomi_camera.git vendor/xiaomi/camera
+git clone --delth=1 https://github.com/Project-Persesx/kernel_xiaomi_sm8250.git -b bpf2 kernel/xiaomi/sm8250
